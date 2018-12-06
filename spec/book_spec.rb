880b0a21879f15a145070c4f239041199304d8a7
Brooke Perry <perrysbrooke@gmail.com>
2018-12-05 21:27:38 -0800

    add book title to books array

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -8,4 +8,16 @@ describe(Book) do
       expect(book1).to(eq(book2))
     end
   end
+
+  describe('.save') do
+    it('add a book title to book array') do
+      book = Book.new({:title  =>"Hobbit"})
+      book.save
+      expect(Book.all).to(eq([book]))
+    end
+  end
+
+
+
+
 end
